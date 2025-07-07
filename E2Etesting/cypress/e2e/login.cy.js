fbf8626880afc9ca6f1ae9b35039f65d02f5745c
ImeshaMalinga <150885477+ImeshaMalinga@users.noreply.github.com>
2025-07-07 21:21:52 +0530

    aaa

--- E2Etesting/cypress/e2e/login.cy.js
+++ E2Etesting/cypress/e2e/login.cy.js
@@ -1,11 +1,15 @@
 it( 'test', ()=>{
     cy.visit('https://www.geonex.site/');
+
+    cy.wait(2000);
      
     cy.get(':nth-child(6) > .bg-blue-600').click();
+    cy.wait(2000);
 
-    cy.get('#email').type('e20242@eng.pdn.ac.lk');
-
-    cy.get('#password').type('malinga123');
+    cy.fixture('login').then((user) => {
+        cy.get('#email').type(user.email);
+        cy.get('#password').type(user.password);
+    });
 
     cy.get('.bg-gray-900').click();
 
